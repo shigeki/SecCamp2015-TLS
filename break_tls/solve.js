1242f3e95dbfbe043e75886c9c895841d75b03ed
Shigeki Ohtsu <ohtsu@iij.ad.jp>
2015-12-12 14:57:44 +0900

    for practice in security mini camp 2015 Hokkaido

--- break_tls/solve.js
+++ break_tls/solve.js
@@ -1,5 +1,5 @@
 var DataReader = require('seccamp2015-data-reader').DataReader;
-var SecCampTLS = require('seccamp2015-tls');
+var SecCampTLS = require('../index.js');
 var fs = require('fs');
 var crypto = require('crypto');
 var handshake = require(__dirname + '/handshake.json');
@@ -20,6 +20,7 @@ var pre_master_secret = crypto.privateDecrypt(
   }, encrypted_pre_master_secret);
 // objtain keyblock
 var keyblock = SecCampTLS.KDF(pre_master_secret, client_random, server_random);
+console.log(keyblock);
 // Calculate Sequence Number
 var read_seq = (new Buffer(8)).fill(0);
 read_seq[7] = 0x01;
@@ -33,10 +34,10 @@ var encrypted = frame.slice(8, frame.length - 16);
 var tag = frame.slice(-16);
 // Re-Caluclate record header
 record_header.writeUIntBE(encrypted.length, 3, 2);
-var iv = Buffer.concat([keyblock.client_write_IV, nonce_explicit]);
+var iv = Buffer.concat([keyblock.server_write_IV, nonce_explicit]);
 var aad = Buffer.concat([read_seq, record_header]);
 // Decrypt Application Data
-var decipher = crypto.createDecipheriv('aes-128-gcm', keyblock.client_write_key, iv);
+var decipher = crypto.createDecipheriv('aes-128-gcm', keyblock.server_write_key, iv);
 decipher.setAuthTag(tag);
 decipher.setAAD(aad);
 var clear = decipher.update(encrypted);
